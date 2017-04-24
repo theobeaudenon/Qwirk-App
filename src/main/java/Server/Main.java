654e47d5713582e8af1cc047598d4ea0cbc3864c
theobeaudenon <theo@beaudenon.pro>
2017-04-24 15:07:07 +0200

    commit

--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -4,4 +4,26 @@ package Server;
  * Created by theobeaudenon on 24/04/2017.
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        SocketIOServer logServer = SocketIOServer.newInstance(5000 /*port*/);
+        logServer.setListener(new SocketIOListener() {
+            public void onConnect(Session session) {
+                System.out.println("Connected: " + session);
+            }
+
+            public void onMessage(Session session, ByteBuf message) {
+                System.out.println("Received: " + message.toString(CharsetUtil.UTF_8));
+                message.release();
+            }
+
+            public void onDisconnect(Session session) {
+                System.out.println("Disconnected: " + session);
+            }
+        });
+        logServer.start();
+    }
+
 }
